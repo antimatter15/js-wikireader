7edbaf78ab92ee057a561e588cbb539013018405
antimatter15 <antimatter15@gmail.com>
2011-04-19 00:02:58 -0400

    works awesomely

--- app/ui.js
+++
@@ -1,6 +0,0 @@
-autocomplete(document.getElementById('search'), document.getElementById('autocomplete'), function(value){
-	return ["blah","asdfasdf","meow","kittens"]
-}, function(query){
-	console.log(query);
-	document.getElementById('title').innerText = query;
-})
